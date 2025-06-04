5ee9a9108abd7e0261cbafe29c84b6539d7ce72c
github Takashi-Horihata <github Takashi-Horihata@kccs.co.jp>
2025-06-04 12:42:21 +0900

    初回コミット

---
+++ ObjStdEx0101/src/store/Product.java
@@ -0,0 +1,41 @@
+package store;
+
+public class Product {
+	private static final String STORE_NAME = "速水PC販売店";
+	private static final int DEFAULT_CHANGE_AMT = 1000;
+//	private static int instanceCnt = 0;
+	private String name;
+	private int price;	
+	
+	public Product(String name, int price) {
+		this.name = name;
+		this.price = price;
+//		instanceCnt++;
+	}
+	
+	public Product (String name) {
+		this(name, 0);
+	}
+	
+	public static void displayStoreName() {
+		System.out.println("■店舗名：" + STORE_NAME);
+	}
+	
+	public int up(int upPrice) {
+		return price += upPrice;
+	}
+	public int up() {
+		return this.up(DEFAULT_CHANGE_AMT);
+	}
+	
+	public int down(int downPrice) {
+		return price -= downPrice;
+	}
+	public int down() {
+		return this.down(DEFAULT_CHANGE_AMT);
+	}
+	
+	public void display() {
+		System.out.println(name + ":" + price + "円");
+	}
+}
